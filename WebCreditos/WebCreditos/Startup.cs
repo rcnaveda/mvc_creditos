26fc40574cf503a62eb2bb830e6f2890c9d5a45a
jimmeza <jim.meza@outlook.com>
2016-03-05 15:02:06 -0500

    Modificacion 05-Mar-2016

---
+++ WebCreditos/WebCreditos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebCreditos.Startup))]
+namespace WebCreditos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
